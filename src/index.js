05ef6dd4388c37bcc4249fdff25d7407a2479036
Fran Palmero <sweetydesigns@gmail.com>
2024-05-25 01:18:17 +0200

    Rework user vote limit
    - Add joke limit to 2 votes.
    - Add vote buttons background colors logic when selection clicked.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -11,7 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 // Create generateJokes function
 const jokes = document.querySelector('.joke');
 const btn = document.getElementById('btn');
-const generateJokes = () => __awaiter(void 0, void 0, void 0, function* () {
+const generateJokes = () => __awaiter(this, void 0, void 0, function* () {
     try {
         const setHeader = {
             headers: {
@@ -24,13 +23,14 @@ const generateJokes = () => __awaiter(void 0, void 0, void 0, function* () {
         const data = yield res.json();
         console.log(data);
         jokes.innerHTML = data.joke;
+        userVotes = 0;
     }
     catch (error) {
         console.log('The error is ${error}');
     }
 });
 // Create generateJokes2 function
-const generateJokes2 = () => __awaiter(void 0, void 0, void 0, function* () {
+const generateJokes2 = () => __awaiter(this, void 0, void 0, function* () {
     try {
         // Fetching Api with async await
         const url = "https://api.chucknorris.io/jokes/random";
@@ -38,6 +38,7 @@ const generateJokes2 = () => __awaiter(void 0, void 0, void 0, function* () {
         const data = yield res.json();
         console.log(data);
         jokes.innerHTML = data.value;
+        userVotes = 0;
     }
     catch (error) {
         console.log('The error is ${error}');
@@ -46,30 +47,72 @@ const generateJokes2 = () => __awaiter(void 0, void 0, void 0, function* () {
 // Call random to Joke1 or Joke2
 const jokeCall = () => {
     const random = Math.trunc(Math.random() * 2);
-    console.log(random);
     if (random <= 0)
         generateJokes();
     else
         generateJokes2();
     backgroundChange();
+    document.getElementById("voteRes").innerHTML = "";
+    resetColor();
 };
-// Calling jokeCall function
+// Calling button jokes
 btn.addEventListener('click', jokeCall);
 jokeCall();
-;
-const reportJokes = [];
+let reportJokes = [];
+let userVotes = 0;
 function report(score) {
-    let report = {
-        joke: jokes.innerHTML,
-        score: score,
-        date: new Date().toISOString(),
-    };
-    reportJokes.push(report);
-    console.table(reportJokes);
+    const index = reportJokes.findIndex((item) => item.joke === jokes.innerHTML);
+    if (index !== -1 && userVotes === 1) {
+        // Report 2nd vote, update Array score
+        reportJokes[index].score = score;
+        reportJokes[index].date = new Date().toISOString();
+        document.getElementById("voteRes").innerHTML = "Vot actualitzat amb èxit!";
+        console.table(reportJokes);
+        resetColor();
+    }
+    else if (userVotes <= 1) {
+        // Report 1st vote to Array
+        const report = {
+            joke: jokes.innerHTML,
+            score: score,
+            date: new Date().toISOString(),
+        };
+        reportJokes.push(report);
+        document.getElementById("voteRes").innerHTML = "Gràcies, vot realitzat amb èxit!";
+        console.table(reportJokes);
+    }
+    else {
+        // Show max 2 vote
+        document.getElementById("voteRes").innerHTML = "Límit de 2 vots realitzats per acudit!";
+        console.log('Max 2 Votes');
+    }
+}
+// Handle vote buttons click
+function handleClick(btnId, color) {
+    if (userVotes <= 1) {
+        document.getElementById(btnId).style.backgroundColor = color;
+        userVotes++;
+    }
+}
+// Asign event listeners to vote buttons
+const buttons = [
+    { id: 'btn1', color: 'red' },
+    { id: 'btn2', color: 'yellow' },
+    { id: 'btn3', color: 'green' }
+];
+buttons.forEach(button => {
+    document.getElementById(button.id).addEventListener('click', () => {
+        handleClick(button.id, button.color);
+    });
+});
+function resetColor() {
+    const buttons = ['btn1', 'btn2', 'btn3'];
+    buttons.forEach(id => {
+        document.getElementById(id).style.backgroundColor = 'white';
+    });
 }
 // Weather API
-let tempResp;
-const weather = () => __awaiter(void 0, void 0, void 0, function* () {
+const weather = () => __awaiter(this, void 0, void 0, function* () {
     try {
         const response = yield fetch('https://api.openweathermap.org/data/2.5/weather?lat=41.389&lon=2.159&units=metric&appid=e7704bc895b4a8d2dfd4a29d404285b6');
         const data = yield response.json();
@@ -83,10 +126,11 @@ const weather = () => __awaiter(void 0, void 0, void 0, function* () {
         console.log(data);
     }
     catch (error) {
-        console.log("Error calling API:", error);
+        console.log('Error calling API:, ${error}');
     }
 });
 weather();
+// Generate random background function
 function backgroundChange() {
     const backPic = [
         "img/blob.svg",
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ const generateJokes = async () => {
     const data = await res.json();
     console.log(data);
     jokes.innerHTML = data.joke;
+    userVotes = 0;
     }
     catch (error) {
         console.log('The error is ${error}');
@@ -30,6 +31,7 @@ const generateJokes2 = async () => {
       const data = await res.json();
       console.log(data);
       jokes.innerHTML = data.value;
+      userVotes = 0;
   }
   catch (error) {
       console.log('The error is ${error}');
@@ -39,33 +41,80 @@ const generateJokes2 = async () => {
 // Call random to Joke1 or Joke2
 const jokeCall = () => {
   const random : number = Math.trunc(Math.random() * 2);
-  console.log(random);
   if (random <= 0) generateJokes();
   else generateJokes2();
   backgroundChange();
+  document.getElementById("voteRes").innerHTML = "";
+  resetColor();
 };
 
-// Calling jokeCall function
+// Calling button jokes
 btn.addEventListener('click', jokeCall);
 jokeCall();
 
-// Report Jokes
-interface jokeResult { joke: string; score: number; date: string };
-const reportJokes: jokeResult[] = [];
+// Report Jokes score
+interface jokeResult {
+  joke: string;
+  score: number;
+  date: string;
+}
 
-  function report(score: number) :void {
-    let report: jokeResult = {
-      joke : jokes.innerHTML,
+let reportJokes: jokeResult[] = [];
+let userVotes: number = 0;
+
+function report(score: number): void {
+  const index = reportJokes.findIndex((item) => item.joke === jokes.innerHTML);
+  if (index !== -1 && userVotes === 1) {
+    // Report 2nd vote, update Array score
+    reportJokes[index].score = score;
+    reportJokes[index].date = new Date().toISOString();
+    document.getElementById("voteRes").innerHTML = "Vot actualitzat amb èxit!";
+    console.table(reportJokes);
+    resetColor();
+  }
+  else if (userVotes <= 1) {
+    // Report 1st vote to Array
+    const report = {
+      joke: jokes.innerHTML,
       score: score,
       date: new Date().toISOString(),
     };
     reportJokes.push(report);
+    document.getElementById("voteRes").innerHTML = "Gràcies, vot realitzat amb èxit!";
     console.table(reportJokes);
   }
+  else {
+  // Show max 2 vote
+  document.getElementById("voteRes").innerHTML = "Límit de 2 vots realitzats per acudit!";
+  console.log('Max 2 Votes');
+  }
+}
+// Handle vote buttons click
+function handleClick(btnId, color) {
+  if (userVotes <= 1 ) {
+    document.getElementById(btnId).style.backgroundColor = color;
+    userVotes++;
+  }
+}
+// Asign event listeners to vote buttons
+const buttons = [
+  { id: 'btn1', color: 'red' },
+  { id: 'btn2', color: 'yellow' },
+  { id: 'btn3', color: 'green' }
+];
+buttons.forEach(button => {
+  document.getElementById(button.id).addEventListener('click', () => {
+      handleClick(button.id, button.color);
+  });
+});
+function resetColor() {
+  const buttons = ['btn1', 'btn2', 'btn3'];
+  buttons.forEach(id => {
+      document.getElementById(id).style.backgroundColor = 'white';
+  });
+}
 
 // Weather API
-let tempResp: string;
-
 const weather = async () => {
   try {
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=41.389&lon=2.159&units=metric&appid=e7704bc895b4a8d2dfd4a29d404285b6');
@@ -73,20 +122,20 @@ const weather = async () => {
     const tempResp = data.main.temp;
     const ico = data.weather[0].icon;
 
-    document.getElementById('tempResp')!.innerHTML = tempResp.toFixed(1) + 'º' +'C';
-
+    document.getElementById('tempResp')!.innerHTML = tempResp.toFixed(1) + 'º' + 'C';
     const icoURL = `http://openweathermap.org/img/w/${ico}.png`;
     const icoElement = document.createElement('img');
     icoElement.src = icoURL;
-    document.getElementById('icoWeather')!.appendChild(icoElement);
 
+    document.getElementById('icoWeather')!.appendChild(icoElement);
     console.log(data);
   } catch (error) {
-    console.log("Error calling API:", error);
+    console.log('Error calling API:, ${error}');
   }
 };
 weather();
 
+// Generate random background function
 function backgroundChange(): void {
   const backPic: string[] = [
     "img/blob.svg",
@@ -96,11 +145,9 @@ function backgroundChange(): void {
     "img/blob4.svg",
     "img/blob5.svg",
   ];
-
   const randomOrder: number = Math.floor(
     Math.random() * backPic.length
   );
   const selectedPic: string = backPic[randomOrder];
-
   document.body.style.backgroundImage = `url(${selectedPic})`;
 }
